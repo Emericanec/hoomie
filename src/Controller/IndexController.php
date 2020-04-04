aae1172bc78d512a25f572ff8bf25487cc859571
andrey <aasvetlichniy@gmail.com>
2020-04-04 17:43:22 +0300

    add yarn encore to deploy

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -17,7 +17,7 @@ class IndexController extends AbstractController
     public function index(): Response
     {
         return $this->render('index/index.html.twig', [
-            'name' => 'Andrey'
+            'name' => 'Homie'
         ]);
     }
 }
