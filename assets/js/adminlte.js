8edaef8df509ad87c84ef3adac94a879038c945d
andrey <aasvetlichniy@gmail.com>
2020-04-10 00:43:17 +0300

    logo

--- assets/js/adminlte.js
+++ assets/js/adminlte.js
@@ -1,4 +1,5 @@
 import jQuery from 'jquery';
+import '../css/fonts.scss'
 import '../../vendor/almasaeed2010/adminlte/dist/css/adminlte.min.css'
 import '../../vendor/almasaeed2010/adminlte/plugins/fontawesome-free/css/all.css'
 import '../../vendor/almasaeed2010/adminlte/dist/js/adminlte.min'
