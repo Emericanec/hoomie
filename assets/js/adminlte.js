c772f5edbfaf84edf7c6c5cb2aaba53564311d52
andrey <aasvetlichniy@gmail.com>
2020-04-10 10:57:41 +0300

    migration

--- assets/js/adminlte.js
+++ assets/js/adminlte.js
@@ -3,3 +3,5 @@ import '../css/fonts.scss'
 import '../../vendor/almasaeed2010/adminlte/dist/css/adminlte.min.css'
 import '../../vendor/almasaeed2010/adminlte/plugins/fontawesome-free/css/all.css'
 import '../../vendor/almasaeed2010/adminlte/dist/js/adminlte.min'
+
+$(document).on('click', '#sidebar-overlay', () => { $('.nav-link').click();});
